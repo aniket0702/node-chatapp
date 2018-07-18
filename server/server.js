8edd1de8902930df7d15652c13fafb5675908d03
aniket0702 <aniketagarwal11133@yahoo.com>
2018-07-18 17:55:56 +0530

    add connet and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
 const express = require('express');
 const publicPath = path.join(__dirname+'/../public');
+
 var app = express();
+var server = http.createServer(app);
+
 const port = process.env.PORT || 3000;
-console.log(__dirname+'/../public');
-console.log(publicPath);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
-app.listen(port, () => {
+
+io.on('connection', (socket) => {
+  console.log('new connection');
+  socket.on('disconnect', () => {
+    console.log('client disconnected');
+  })
+});
+
+
+
+server.listen(port, () => {
   console.log('app is up', port);
 });
