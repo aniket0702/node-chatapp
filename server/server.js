260f279e921834cc6a7c66bbd2ab30eb159e56ef
aniket0702 <aniketagarwal11133@yahoo.com>
2018-07-19 10:41:14 +0530

    add newMessage and createMessage

--- server/server.js
+++ server/server.js
@@ -7,23 +7,44 @@ const publicPath = path.join(__dirname+'/../public');
 var app = express();
 var server = http.createServer(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('new connection');
-
+  //
+  // socket.emit('newMessage', {
+  //   from: 'mike',
+  //   text: 'hello',
+  //   createAt: new Date()
+  // });
   socket.emit('newMessage', {
-    from: 'mike',
-    text: 'hello',
-    createAt: new Date()
+    from: 'Admin',
+    text: 'Welcome to the chat room',
+    createdAt: new Date().getTime()
   });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User Joined',
+    createdAt: new Date().getTime()
+  })
 
   socket.on('createMessage', (message) => {
     console.log('createEmail', message);
-  })
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  });
 
   socket.on('disconnect', () => {
     console.log('client disconnected');
