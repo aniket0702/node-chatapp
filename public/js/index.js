165877aaacdbe397462359b1fd3063024b90c0fe
aniket0702 <aniketagarwal11133@yahoo.com>
2018-07-20 12:56:07 +0530

    build ui and build form

---
+++ public/js/index.js
@@ -0,0 +1,46 @@
+var socket = io();
+socket.on('connect', () => {
+  console.log('connected to server');
+  // socket.emit('createMessage', {
+  //   to: 'aniket',
+  //   text: 'hello'
+  // });
+});
+
+socket.on('disconnect', () => {
+  console.log('disconnected');
+});
+
+socket.on('newMessage', (message) => {
+  console.log('new message', message);
+  var li =jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'user',
+    text: jQuery('[name=message]').val()
+  }, function() {
+
+  });
+});
+
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function(){
+  if(!navigator.geolocation){
+    return alert('Geolocation for this brower not suppoted');
+  }
+  navigator.geolocation.getCurrentPosition(function(position) {
+    socket.emit('createLocationMessage', {
+      longitude: position.coords.longitude,
+      latitude: position.coords.latitude
+    });
+    console.log(position);
+  }, function() {
+    alert('unable to fetch location');
+  })
+})
